b621e6c003a393b894e36765e4bf49e69544942a
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2024-10-16 17:09:49 +0900

    登録、ログイン、ログアウト機能の追加

--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from user.models import CustomUser
+from django.contrib.auth import login, logout
 
-# Create your views here.
+def signup(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    new_user = CustomUser(usrename=username, password=password)
+    new_user.save()
+    return HttpResponse('ユーザの作成に成功しました')
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    try:
+        user = CustomUser.object.get(username=username)
+    except CustomUser.DoesNotExist:
+        return HttpResponse('ログインに失敗しました')
+    
+    if user.password == password:
+        login(request, user)
+        return HttpResponseRedirect('/')
+    else:
+        return HttpResponse('ログインに失敗しました')
+    
+def signout(request):
+    logout(request)
+    return HttpResponseRedirect('/')    
