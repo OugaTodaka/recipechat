a18bca84e11aff3ca601f3f2c44cb7d1d634a9a7
shinnosukegoto <xinzhizhuw@gmail.com>
2024-10-24 18:34:59 +0900

    10/24

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,24 @@
 from django.views.generic import FormView
 from .models import Chat
+from django.urls import reverse_lazy
+from .form import ChatForm
 
 class ChatView(FormView):
     template_name = "chat/chat.html"
-    model = Chat
-    def get_queryset(self):
-        return Chat.objects.filter(user=self.request.user).order_by("send_at")
+    success_url = reverse_lazy('')
+    form_class = ChatForm
+#     def get_queryset(self):
+#         return Chat.objects.filter(user=self.request.user).order_by("send_at")
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         print(context)
         return context
-    def form_valid(self, form):
+    def form_valid(self,form):
+        message = form.cleaned_data['chat']
+        user = self.request.user.username
+        detail = message
+
+
+
+
 
