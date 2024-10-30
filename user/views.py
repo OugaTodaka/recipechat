c921eb1c1d078e178bb0e064e295b2abe665fe6b
OugaTodaka <fko2347017@stu.o-hara.ac.jp>
2024-10-30 17:43:28 +0900

    user登録ログインろぐあうと

--- user/views.py
+++ user/views.py
@@ -1,30 +1,52 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.template import loader
 from user.models import CustomUser
 from django.contrib.auth import login, logout
+from django.contrib.auth.hashers import check_password,make_password
+
+def signin(request):
+    template = loader.get_template("user/signin.html")
+    return HttpResponse(template.render(None, request))
 
 def signup(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    new_user = CustomUser(usrename=username, password=password)
-    new_user.save()
-    return HttpResponse('ユーザの作成に成功しました')
+    template = loader.get_template("user/signup.html")
+    return HttpResponse(template.render(None, request))
 
-def login(request):
+
+def signup_sys(request):
+    try:
+        username = request.POST['username']
+        print("ゆーざーーーーーーーーーーーーーーーーーー")
+        password = request.POST['password']
+        print("ぱすーーーーーーーーーーーーーーーーーーーーーーーーー")
+        pass_word = make_password(password)
+        print("はっしゅーーーーーーーーーーーーーーーーーーー")
+        new_user = CustomUser(username=username, password=pass_word)
+        print("かすたむゆーざーーーーーー")
+        new_user.save()
+        print("ほぞんーーーーーーーーーーーー")
+        login(request, new_user)
+        print("ろぐいんーーーーーーーーーーーーーーーーーー")
+        return HttpResponseRedirect('/')
+    except:
+        return HttpResponse('ユーザ作成に失敗しました')
+
+def signin_sys(request):
     username = request.POST['username']
     password = request.POST['password']
 
     try:
-        user = CustomUser.object.get(username=username)
+        user = CustomUser.objects.get(username=username)
     except CustomUser.DoesNotExist:
         return HttpResponse('ログインに失敗しました')
     
-    if user.password == password:
+    if check_password(password, user.password):
         login(request, user)
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect("/")
     else:
         return HttpResponse('ログインに失敗しました')
     
-def signout(request):
+def signout_sys(request):
     logout(request)
-    return HttpResponseRedirect('/')    
+    return HttpResponseRedirect('/user/signin')
