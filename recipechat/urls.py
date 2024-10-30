843273e0c7e6caaf9ad17f1c9eee7bd7d89b9d11
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2024-10-30 16:34:08 +0900

    urls修正

--- recipechat/urls.py
+++ recipechat/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chat.urls')),
-    # path('user/',include('user.urls')),
+    path('user/',include('user.urls')),
     path('favorite/',include('favorite.urls')),
 ]
