8d5dab24f4ccee94b908f2b833cc1c37ed584c3c
OugaTodaka <fko2347017@stu.o-hara.ac.jp>
2024-11-11 18:00:19 +0900

    お気に入り追加機能

--- favorite/views.py
+++ favorite/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Favorite
+from chat.models import Chat
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -15,6 +16,16 @@ class FavoriteView(LoginRequiredMixin,ListView):
         context = super().get_context_data(**kwargs)
         print(context)
         return context
+
+def favorite_add(request,chat_id):
+    chat = Chat.objects.get(id=chat_id)
+    Favorite.objects.create(user=request.user,
+                            title=chat.detail,
+                            link=chat.link,
+                            img_link=chat.img_link
+                            )
+    return HttpResponseRedirect(reverse('chat:chat'))
+
 
 def favorite_delete(request,favorite_id):
     favorite = Favorite.objects.get(id=favorite_id)
